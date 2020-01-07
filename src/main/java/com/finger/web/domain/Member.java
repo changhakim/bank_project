b66b343249228593f177e5d0d243b09b14d88170
MIN <MIN@DESKTOP-7MU4R9E>
2020-01-07 09:24:29 +0900

    20200106gettersetter

--- src/main/java/com/finger/web/domain/Member.java
+++ src/main/java/com/finger/web/domain/Member.java
@@ -11,5 +11,70 @@ import lombok.Data;
 public class Member {
 	private String mid, password,address, name, phone, mail,account,birth;
 
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	
+	
 
 }
